8167a666f1e237e5d030e557df7fae0dcdbba720
student5 <student5@jh-mba158-05.d214.org>
2017-06-09 09:31:11 -0500

    help page

---
+++ cuteCat/helpVC.swift
@@ -0,0 +1,24 @@
+//
+//  helpVC.swift
+//  cuteCat
+//
+//  Created by student5 on 6/9/17.
+//  Copyright © 2017 Alisha Fong. All rights reserved.
+//
+
+import UIKit
+
+class helpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func dismiss(_ sender: Any)
+    {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
